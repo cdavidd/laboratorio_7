9f3e8ee4fbe139f651abcb82955b173bf39619bd
cdavidd <2133561@linux55.labinfo.is.escuelaing.edu.co>
2018-10-17 16:13:30 -0500

    arrreglos

---
+++ src/test/java/edu/eci/pdsw/tests/TestBase.java
@@ -0,0 +1,12 @@
+package edu.eci.pdsw.tests;
+
+import org.junit.Before;
+import org.mybatis.guice.XMLMyBatisModule;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+
+public class TestBase {
+
+}
